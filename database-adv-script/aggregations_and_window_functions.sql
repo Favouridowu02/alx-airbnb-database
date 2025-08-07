9dd82dc37d0da4c4123b4951554a4f7215cbefc1
Favour Idowu <favouridowu02@gmail.com>
2025-08-07 12:04:06 +0100

    feat: Add SQL queries for user booking counts and property ranking
    Add query to count total bookings per user using COUNT and GROUP BY
    Add query to rank properties by total bookings using RANK() window function
    Place queries in booking_queries.sql for easy reference and reuse

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,21 @@
+-- This Document contains a query that find the total number of bookings made by each user, using the COUNT function and GROUP BY clause
+-- 
+
+-- Query 1: Total number of bookings made by each user
+SELECT
+  user_id,
+  COUNT(*) AS total_bookings
+FROM
+  bookings
+GROUP BY
+  user_id;
+
+-- Query 2: Rank properties based on the total number of bookings they have received
+SELECT
+  property_id,
+  COUNT(*) AS total_bookings,
+  RANK() OVER (ORDER BY COUNT(*) DESC) AS property_rank
+FROM
+  bookings
+GROUP BY
+  property_id;
